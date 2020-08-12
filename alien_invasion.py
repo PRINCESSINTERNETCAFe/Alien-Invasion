708cfa403b424d3abb8d9b11806a0577e2862f48
David Gallardo Nieto-Sandoval <d.gallardo.ns@gmail.com>
2020-08-12 12:09:35 +0100

    Created bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,9 @@
 import sys
-
 import pygame as pyg
 
 from settings import Settings
-
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     #Overall class to manage game assets and behaviour
@@ -17,6 +16,7 @@ class AlienInvasion:
         pyg.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pyg.sprite.Group()
 
         #Set background colour
         self.bg_color = self.settings.bg_color
@@ -26,7 +26,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self._update_screen()
+            self._update_bullets()
+            self._update_screen()       
     
     def _check_events(self):
     #Reponds to keypresses and mouse events
@@ -34,27 +35,54 @@ class AlienInvasion:
                 if event.type == pyg.QUIT:
                     sys.exit()
                 elif event.type == pyg.KEYDOWN:
-                    if event.key == pyg.K_RIGHT:
-                        #Move the ship to the right
-                        self.ship.moving_right = True
-                    elif event.key == pyg.K_LEFT:
-                        #Move the ship to the left
-                        self.ship.moving_left = True
-
+                    self._check_keydown_events(event)
                 elif event.type == pyg.KEYUP:
-                    if event.key == pyg.K_RIGHT:
-                        #Stop moving the ship
-                        self.ship.moving_right = False
-                    elif event.key == pyg.K_LEFT:
-                        #Stop moving the ship
-                        self.ship.moving_left = False
+                    self._check_keyup_events(event) 
 
+    def _check_keydown_events(self, event):
+        #Respond to key presses
+        if event.key == pyg.K_RIGHT:
+            #Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pyg.K_LEFT:
+            #Move the ship to the left
+            self.ship.moving_left = True
+        elif event.key == pyg.K_ESCAPE:
+            sys.exit()
+        elif event.key == pyg.K_SPACE:
+            self._fire_bullet()
     
+    def _check_keyup_events(self, event):
+        #Respond to key reeleases
+        if event.key == pyg.K_RIGHT:
+            #Stop moving the ship
+            self.ship.moving_right = False
+        elif event.key == pyg.K_LEFT:
+            #Stop moving the ship
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        #Create a new bullet and add it to the bullets group
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+    #Get rid of bullets and get rid of bullets
+            self.bullets.update()       
+
+            #Get rid of bullets that have disappeared  
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+
     def _update_screen(self):
     #Update images on the screen and flip to the new screen
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
-            
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
+
             # #Make the most recently drawn screen visible
             pyg.display.flip()
 
