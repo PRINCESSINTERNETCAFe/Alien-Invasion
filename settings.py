8c2f84ae2af882c7691576cb3d2e61a199755081
David Gallardo Nieto-Sandoval <d.gallardo.ns@gmail.com>
2020-08-19 14:39:48 +0100

    Ship lives added

--- settings.py
+++ settings.py
@@ -11,16 +11,17 @@ class Settings:
 
         #Ship settings
         self.ship_speed = 2.5
+        self.ship_limit = 3
 
         #Bullets settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0, 255, 255)
         self.bullets_allowed = 10
 
         #Alien settings
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.alien_speed = 5
+        self.fleet_drop_speed = 5
         # Fleet direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
